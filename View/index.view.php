d200f6adad16e40bdcaa64f1461088b9c1a4185b
Julien Scourneau <scourneaujulien@outlook.com>
2022-10-14 10:35:45 +0200

    17.Update view

--- View/index.view.php
+++ View/index.view.php
@@ -1,20 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-<nav>
-    <ul>
-        <li><a href="/about">About page</a></li>
-        <li><a href="/contact">Contact page</a></li>
-    </ul>
-</nav>
+<?php require ('partials/head.php'); ?>
 
 <ul>
     <?php foreach ($tasks
@@ -29,7 +13,4 @@
         </li>
     <?php endforeach; ?>
 </ul>
-
-
-</body>
-</html>
+<?php require('partials/footer.php');
---
+++ View/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/about">About page</a></li>
+        <li><a href="/about/culture">About our Culture</a></li>
+        <li><a href="/contact">Contact page</a></li>
+    </ul>
+</nav>
