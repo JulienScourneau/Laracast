42e3144058e4f7cfe3646f027bc6a0760a37994d
Julien Scourneau <scourneaujulien@outlook.com>
2022-10-24 12:08:59 +0200

    Create news factories

--- 02.Laravel/database/seeders/DatabaseSeeder.php
+++ 02.Laravel/database/seeders/DatabaseSeeder.php
@@ -17,78 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name'=>'John Doe'
         ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
+        Post::factory(5)->create([
+            'user_id'=>$user->id
         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur aut autem dicta dignissimos eaque,
-            id illo incidunt maiores molestiae necessitatibus,
-            nemo nihil nostrum perferendis quasi quia quisquam similique unde. Quod?'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur aut autem dicta dignissimos eaque,
-            id illo incidunt maiores molestiae necessitatibus,
-            nemo nihil nostrum perferendis quasi quia quisquam similique unde. Quod?'
-        ]);
-        Post::create([
-
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur aut autem dicta dignissimos eaque,
-            id illo incidunt maiores molestiae necessitatibus,
-            nemo nihil nostrum perferendis quasi quia quisquam similique unde. Quod?'
-        ]);;
-        Post::create([
-
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Second Personal Post',
-            'slug' => 'my-second-personal-post',
-            'excerpt' => 'Lorem ipsum',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur aut autem dicta dignissimos eaque,
-            id illo incidunt maiores molestiae necessitatibus,
-            nemo nihil nostrum perferendis quasi quia quisquam similique unde. Quod?'
-        ]);
-
     }
 }
