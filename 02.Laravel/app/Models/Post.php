632e95c09ce7a6887ac799d62857e0a9fb52d91f
Julien Scourneau <scourneaujulien@outlook.com>
2022-10-26 11:35:41 +0200

    02.laravel: Create Post controller and update navigation

--- 02.Laravel/app/Models/Post.php
+++ 02.Laravel/app/Models/Post.php
@@ -12,7 +12,14 @@ class Post extends Model
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
 
-    //protected $fillable = ['title', 'excerpt', 'body','id'];
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) => $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
+
+        $query->when($filters['category'] ?? false, fn($query, $category) => $query->whereHas('category', fn($query) => $query->where('slug', $category)));
+    }
 
     public function category()
     {
